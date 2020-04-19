af98bc05fd66c1f3cd8a7a87e6379a76c14351bd
Jude Paulemon <jvp3923@rit.edu>
2020-04-19 15:08:34 -0400

    trying the matric with a coordinate Object instead of bare String

--- src/lasers/model/Safe.java
+++ src/lasers/model/Safe.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import lasers.ptui.LasersPTUI;
@@ -12,6 +13,9 @@ public class Safe {
     String[][] safeMatrix;
     String[][] safeMatrixSol;
     LasersPTUI lasersPTUI;
+
+    Coordinate[][] safeMatrixC;
+    Coordinate[][] safeMatrixSolC;
 
     String LASER = "L";
     String BEAM = "*";
@@ -46,6 +50,9 @@ public class Safe {
                     col = Integer.parseInt(size.substring(2, 3));
                     safeMatrix = new String[row][col];
                     safeMatrixSol = new String[row][col];
+
+                    safeMatrixC =  new Coordinate[row][col];
+                    safeMatrixSolC =  new Coordinate[row][col];
                 } else if (counter == 1) {
 
                     for (int r = 0; r < row; r++) {
@@ -53,6 +60,8 @@ public class Safe {
                         int c2 = 0;
                         for (int c = 0; c < col + col - 1; c += 2) {
                             safeMatrix[r][c2] = dataRow.substring(c, c + 1);
+
+                            safeMatrixC[r][c2] = new Coordinate(r, c2, dataRow.substring(c, c + 1));
                             c2++;
                         }
                     }
@@ -69,6 +78,7 @@ public class Safe {
                         int c2 = 0;
                         for (int c = 2; c < (col + 2) + (col - 1); c += 2) {
                             safeMatrixSol[r][c2] = dataRow.substring(c, c + 1);
+                            safeMatrixSolC[r][c2] = new Coordinate(r, c2, dataRow.substring(c, c + 1));
                             c2++;
                         }
                     }
@@ -87,13 +97,13 @@ public class Safe {
 
     public void addLaser(int row, int col) {
         String response;
-        if (safeMatrix[row][col].equals(FREE_SPOT) || safeMatrix[row][col].equals(BEAM)) {
-            safeMatrix[row][col] = LASER;
-            response = "Laser added at: (" + row + ", " + col + ")";
-            beamRow(row, col, 0,1);
-            beamCol(row, col, 0,1);
-
-        } else {
+            boolean status = safeMatrixC[row][col].updateElement(LASER);
+            
+            if(status){
+                response = "Laser added at: (" + row + ", " + col + ")";
+                beamRow(row, col, 0,1,safeMatrixC[row][col]);
+                beamCol(row, col, 0,1);
+            }else {
             response = "Error adding laser at: (" + row + ", " + col + ")";
         }
 
@@ -102,8 +112,8 @@ public class Safe {
 
     public void removeLaser(int row, int col) {
         String response;
-        if (safeMatrix[row][col].equals(LASER)) {
-            safeMatrix[row][col] = FREE_SPOT;
+            boolean status = safeMatrixC[row][col].updateElement(FREE_SPOT);
+            if(status){
             response = "Laser removed at: (" + row + ", " + col + ")";
             beamRow(row, col, 0,-1);
             beamCol(row, col, 0,-1);
@@ -123,27 +133,31 @@ public class Safe {
 
     }
 
-    public void beamRow(int row, int col, int direction, int action) {
+    public void beamRow(int row, int col, int direction, int action, Coordinate laser) {
         if (row >= 0 && row < safeMatrix.length) {
-
+            boolean status;
             String cell = safeMatrix[row][col];
             if (direction == 0) {
-                beamRow(row + 1, col, 1, action);
-                beamRow(row - 1, col, -1, action);
+                beamRow(row + 1, col, 1, action,laser);
+                beamRow(row - 1, col, -1, action,laser);
             }
 
             if (action > 0) {
+
+
+                status = safeMatrixC[row][col].updateElement(BEAM,LASER);
+
+                
+
                 if (cell.equals(FREE_SPOT) || cell.equals(BEAM)) {
                     safeMatrix[row][col] = BEAM;
-                        beamRow(row+direction, col, direction, action);
+                        beamRow(row+direction, col, direction, action,laser);
 
                     } 
                 }else{
-                    if (cell.equals(BEAM) ) {
+                    if (cell.equals(BEAM)) {
                         safeMatrix[row][col] = FREE_SPOT;
-                        beamRow(row+direction, col, direction, action);
-                    }else if(cell.equals(LASER)){
-                        beamRow(row, col, direction*-1, action*-1);
+                        beamRow(row+direction, col, direction, action,laser);
                     }
                 }
 
@@ -179,6 +193,64 @@ public class Safe {
 
     public String[][] getMatrix() {
         return safeMatrix;
+    }
+
+    class Coordinate{
+        private int row,col;
+        private String element;
+        private ArrayList<Coordinate> laserDepency = new ArrayList<>();
+
+        public Coordinate(int row,int col,String element){
+            this.row  =  row;
+            this.col = col;
+            this.element = element;
+        }
+
+        public String getELement(){
+            
+            return element;
+        }
+
+        public boolean updateElement(String e){
+            boolean r =  false;
+            if (e.equals(LASER)){
+
+                if(element.equals(FREE_SPOT) || element.equals(BEAM)){
+                    element =  e;
+                    r = true;
+                }
+                
+            }else if(e.equals(FREE_SPOT)){
+                if(element.equals(LASER)){
+                    element = FREE_SPOT;
+                    r = true;
+                }
+            }
+            this.element = e;
+
+            return r;
+        }
+
+        public boolean updateElement(String e, Coordinate laser,int action){
+            boolean r = false;
+
+            if(action > 0){
+                if(element.equals(FREE_SPOT) || element.equals(BEAM)){
+                    element= e;
+                    laserDepency.add(laser);
+                }
+            }
+            
+           
+            
+            
+
+            return false;
+        }
+        public void addDepency(Coordinate c){
+            laserDepency.add(c);
+        }
+
     }
 
 }
